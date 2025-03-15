29f6e02b747b7ddd1e29d103318b763e2ad1672d
victorhsiao5gmailcom <victorhsiao5@gmail.com>
2025-03-15 10:11:12 +0800

    individual nevigation v-0.1

--- Victor/updownbot.py
+++ Victor/updownbot.py
@@ -2,14 +2,17 @@ from api import Command, Simulation, UP, DOWN, MOVE, STOP
 
 def individual_nevigation(stops, floors_assigned, current_floor, resting_floor):
     # only take on floors_assigned when the elevator is empty
-    # OR the elevator will pass the floor
+    # OR the elevator will pass the floor and heading the same direction
     stops = sorted(stops)
     if not stops and (floors_assigned > (current_floor & floors_assigned[""]) or floors_assigned < (current_floor & floors_assigned)):
         print(f"the elevator cannnot accept the floor: {floors_assigned}")
     else:
         stops.append(floors_assigned)
         stops = sorted(stops)
-        if current_floor <
+        if current_floor < stops[0]:
+            pass
+        else:
+            stops.reverse()
     return stops
 
 
