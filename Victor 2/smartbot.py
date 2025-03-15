f0e3956599e344f237488f5fcb91ce561825abf4
victorhsiao5gmailcom <victorhsiao5@gmail.com>
2025-03-15 15:06:14 +0800

    XD

--- Victor 2/smartbot.py
+++ Victor 2/smartbot.py
@@ -1,4 +1,5 @@
 from api import Command, Simulation, UP, DOWN, MOVE, STOP
+from requests_processing import requests_processing
 
 def smartbot():
     """A smart bot that only move when there is a request"""
@@ -13,20 +14,36 @@ def smartbot():
     current_state = simulation.initial_state
     directions = {}
     elevator_data = current_state["elevators"]
+    assigned_requests = []
 
     
-    # Assigning elevator
-    requests = current_state["requests"]
+    # Resting position for each elevator
+    for elevator in current_state["elevators"]:
+        stopping_plan[elevator["id"]] = {
+            "stops": [],
+            "resting_floor": initial_plan[elevator["id"]]["resting_floor"]  # Updated reference here
+        }
+    print(f"Initial Stopping Plan: {stopping_plan}")
+
+    while current_state["running"]:
+        # Request processing
+        requests = current_state["requests"]
+        for request in requests:
+            if request["floor"] in assigned_requests:
+                requests.remove(request)
+        assignment = requests_processing(requests, elevator_data)
+        
+        # command processing
+        commands = []
+        # actions for elevators
+        if assignment:
+            action = MOVE
+
+            for elevator in current_state["elevators"]:
+                if elevator["id"] in assignment.values():
+                    direction = UP if elevator["floor"] < assignment.values() else DOWN
+                    
+                    
     
-    for request in requests:
-        # calculating the distance between the request and the elevators and check for direction
-        distance = {}
-        for elevator in free_elevators:
-            distance = request["floor"] - elevator["floor"]
-            distance[elevator["id"]] = {"distance":distance, "direction":None}
-        for elevator in up_elevators:
-            distance = request["floor"] - elevator["floor"]
-            distance[elevator["id"]] = {"distance":distance, "direction":UP}
-        for elevator in down_elevators:
-            distance = request["floor"] - elevator["floor"]
-            distance[elevator["id"]] = {"distance":distance, "direction":DOWN}
+    
+    commands.append(Command(elevator_id=elevator["id"], direction=direction, action=action))
