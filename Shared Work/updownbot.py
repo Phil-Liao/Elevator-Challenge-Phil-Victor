2bc192fae341dfdbff9dad9f21464e033c575d56
victorhsiao5gmailcom <victorhsiao5@gmail.com>
2025-03-15 10:50:18 +0800

    putting individual_nevigation into a new file

--- Shared Work/updownbot.py
+++ Shared Work/updownbot.py
@@ -1,22 +1,5 @@
 from api import Command, Simulation, UP, DOWN, MOVE, STOP
-
-def individual_nevigation(stops:list[int], floors_assigned:int, current_floor:int) -> list[list]:
-    # only take on floors_assigned when the elevator is empty
-    # OR the elevator will pass the floor and heading the same direction
-    stops = sorted(stops)
-    if not stops and (floors_assigned > (current_floor & floors_assigned) or floors_assigned < (current_floor & floors_assigned)):
-        print(f"the elevator cannnot accept the floor: {floors_assigned}")
-    else:
-        stops.append(floors_assigned)
-        stops = sorted(stops)
-        if current_floor < stops[0]:
-            pass
-        else:
-            stops.reverse()
-    return stops
-
-
-
+from individual_nevigation import individual_nevigation
 
 def updown_bot():
     """An example bot that sends elevators up and down and stops at floors if there are passengers waiting to get on or off"""
